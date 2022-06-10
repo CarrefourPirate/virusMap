9c0e73830f0876f61b7111ce7fad933498f21811
peiratooo <1518457988@qq.com>
2022-06-10 21:05:33 +0800

    change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,18 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server:{
-    // proxy:{
-    //   '/netease':{
-    //     target:'https://c.m.163.com/',
-    //     changeOrigin:true,
-    //     ws:true,
-    //     rewrite:(path)=>path.replace(/^\/netease/,'')
-    //   },
-    //   '/sohu':{
-    //     target:'https://pv.sohu.com/',
-    //     changeOrigin:true,
-    //     rewrite:(path)=>path.replace(/^\/sohu/,'')
-    //   }
-    // }
+    proxy:{
+      '/netease':{
+        target:'https://c.m.163.com/',
+        changeOrigin:true,
+        ws:true,
+        rewrite:(path)=>path.replace(/^\/netease/,'')
+      },
+      '/sohu':{
+        target:'https://pv.sohu.com/',
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/sohu/,'')
+      }
+    }
   }
 })
