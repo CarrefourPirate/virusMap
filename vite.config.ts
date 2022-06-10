41480a808aca0e2c2263ec065a932ba1ce1a2bfc
peiratooo <1518457988@qq.com>
2022-06-10 20:13:00 +0800

    change host

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    proxy:{
-      '/netease':{
-        target:'https://c.m.163.com/',
-        changeOrigin:true,
-        rewrite:(path)=>path.replace(/^\/netease/,'')
-      },
-      '/sohu':{
-        target:'https://pv.sohu.com/',
-        changeOrigin:true,
-        rewrite:(path)=>path.replace(/^\/sohu/,'')
-      }
-    }
-  }
+  // server:{
+  //   proxy:{
+  //     '/netease':{
+  //       target:'https://c.m.163.com/',
+  //       changeOrigin:true,
+  //       rewrite:(path)=>path.replace(/^\/netease/,'')
+  //     },
+  //     '/sohu':{
+  //       target:'https://pv.sohu.com/',
+  //       changeOrigin:true,
+  //       rewrite:(path)=>path.replace(/^\/sohu/,'')
+  //     }
+  //   }
+  // }
 })
