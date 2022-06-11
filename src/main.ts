d6c1bc59e34ceb28586002817956e1cbe365b12d
peiratooo <1518457988@qq.com>
2022-06-11 18:27:45 +0800

    t

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
+App.prototype.$axios = axios
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://peirato-1grqvoqd8f5730ce-1312434121.ap-shanghai.app.tcloudbase.com" : "/api"
 createApp(App).mount('#app')
