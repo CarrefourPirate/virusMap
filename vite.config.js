8e9ab4969df46b0cd1bf270a8cf38de6f873bcf6
peiratooo <1518457988@qq.com>
2022-06-21 16:55:58 +0800

    add ENV

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server:{
-    proxy:{
-      '/netease':{
-        target:'https://c.m.163.com/',
-        changeOrigin:true,
-        ws:true,
-        rewrite:(path)=>path.replace(/^\/netease/,'')
-      },
-      '/sohu':{
-        target:'https://pv.sohu.com/',
-        changeOrigin:true,
-        ws:true,
-        rewrite:(path)=>path.replace(/^\/sohu/,'')
+export default defineConfig(({mode}) => {
+  
+  const config = loadEnv(mode,'./')
+  return {
+    plugins: [vue()],
+    server:{
+      proxy:{
+        '/netease':{
+          target:config.VITE_BASIC_URL,
+          changeOrigin:true,
+          ws:true,
+          rewrite:(path)=>path.replace(/^\/netease/,'')
+        },
+        '/sohu':{
+          target:'https://pv.sohu.com/',
+          changeOrigin:true,
+          ws:true,
+          rewrite:(path)=>path.replace(/^\/sohu/,'')
+        }
       }
-    }
   }
-})
+}})
